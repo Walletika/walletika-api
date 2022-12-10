24cb1d319f9be55ca54bce9354d0cdb34c107bfa
Mahmoud Khalid <darkness20016@gmail.com>
2022-12-10 19:19:44 +0200

    Added loader

---
+++ lib/src/core/loader.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import '../models.dart';
+import 'core.dart';
+
+Future<void> fetchCoinsListed() async {
+  coinsListed.addAll(
+    await fetcher(coinsListedAPI).then<Iterable<CoinListed>>((coins) {
+      return coins.map<CoinListed>((coin) => CoinListed.fromJson(coin));
+    }),
+  );
+
+  if (coinsListed.isNotEmpty) {
+    await cipher.encryptToFile(
+      data: jsonEncodeToBytes(
+        coinsListed.map((coin) => coin.toJson()).toList(),
+      ),
+      path: coinsListedPath,
+      ignoreFileExists: true,
+    );
+  }
+}
+
+Future<void> load(Future<bool> Function() updater) async {
+  if (await File(coinsAESPath).exists()) {
+    coins.addAll(
+      (jsonDecodeFromBytes(
+        await cipher.decryptFromFile(path: coinsAESPath),
+      ) as Map)
+          .cast<String, List<dynamic>>(),
+    );
+  } else {
+    await updater();
+  }
+
+  if (await File(coinsCacheAESPath).exists()) {
+    coinsCache.addAll(
+      (jsonDecodeFromBytes(
+        await cipher.decryptFromFile(path: coinsCacheAESPath),
+      ) as Map)
+          .cast<String, String>(),
+    );
+  }
+
+  if (coinsListed.isEmpty && await File(coinsListedAESPath).exists()) {
+    coinsListed.addAll(
+      (jsonDecodeFromBytes(
+        await cipher.decryptFromFile(path: coinsListedAESPath),
+      ) as List)
+          .map((coin) => CoinListed.fromJson(coin))
+          .toList(),
+    );
+  }
+}
+
+Future<void> dump() async {
+  await cipher.encryptToFile(
+    data: jsonEncodeToBytes(coinsCache),
+    path: coinsCachePath,
+    ignoreFileExists: true,
+  );
+}
