d12a0f582925535a2301422140c850eb6266e2a2
Mahmoud Khalid <darkness20016@gmail.com>
2023-03-17 10:46:45 +0200

    Bypass `CERTIFICATE_VERIFY_FAILED` exception by overrides http client

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -9,6 +9,15 @@ import 'package:path/path.dart' as pathlib;
 import 'core/core.dart';
 import 'models.dart';
 
+class _MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 class WalletikaAPI {
   /// Instance initialization is required
   static Future<void> init({
@@ -22,6 +31,9 @@ class WalletikaAPI {
 
     // Set encryption key
     cipher.setKey(encryptionKey);
+
+    // Bypass `CERTIFICATE_VERIFY_FAILED` exception by overrides http client
+    HttpOverrides.global = _MyHttpOverrides();
 
     // Set file paths
     mainDirectory = directory;
