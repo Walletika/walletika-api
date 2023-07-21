6dab3e55bebfac5b6aa965fa9c80a73ac3bf82a8
Mahmoud Khalid <darkness20016@gmail.com>
2023-07-21 12:13:13 +0200

    Upgrade getCoinID

--- lib/src/core/coin_id.dart
+++ lib/src/core/coin_id.dart
@@ -1,23 +1,21 @@
+import '../controllers/storage/main.dart';
 import '../models.dart';
 import 'core.dart';
 
-/// Extract coinID from coinEntry to get info from CoinGecko by it
+/// Extract coinID to get info from CoinGecko by it
 String? getCoinID(CoinEntry coin) {
   final String symbol = coin.symbol.toUpperCase();
   final String? name = coin.name?.toLowerCase();
   final String? contractAddress = coin.contractAddress?.toLowerCase();
 
-  for (final Map<String, dynamic> item in coins[symbol] ?? []) {
+  for (final Map<String, dynamic> item
+      in StorageController.allCoins.data[symbol] ?? []) {
     if (contractAddress != null &&
-        !item['contracts'].contains(contractAddress)) {
-      continue;
-    }
+        !item[EKey.contracts].contains(contractAddress)) continue;
 
-    if (name != null && name != item['name'].toLowerCase()) {
-      continue;
-    }
+    if (name != null && name != item[EKey.name].toLowerCase()) continue;
 
-    return item['id'];
+    return item[EKey.id];
   }
 
   return null;
