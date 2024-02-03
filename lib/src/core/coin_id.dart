1c1a34c9a2e1c433e5f4c291229feb16cf1a4b71
MahmoudKhalid <darkness20016@gmail.com>
2024-02-03 08:50:55 +0200

    Upgrade getCoinID method to check as a coin

--- lib/src/core/coin_id.dart
+++ lib/src/core/coin_id.dart
@@ -7,9 +7,21 @@ String? getCoinID(CoinEntry coin) {
   final String symbol = coin.symbol.toUpperCase();
   final String? name = coin.name?.toLowerCase();
   final String? contractAddress = coin.contractAddress?.toLowerCase();
+  final List<dynamic> allCoins = StorageController.allCoins.data[symbol] ?? [];
 
-  for (final Map<String, dynamic> item
-      in StorageController.allCoins.data[symbol] ?? []) {
+  // Check as a coin without smart contract
+  if (contractAddress == null) {
+    for (final Map<String, dynamic> item in allCoins) {
+      if (item[EKey.contracts].isNotEmpty) continue;
+
+      if (name != null && name != item[EKey.name].toLowerCase()) continue;
+
+      return item[EKey.id];
+    }
+  }
+
+  // Check as a token by smart contract
+  for (final Map<String, dynamic> item in allCoins) {
     if (contractAddress != null &&
         !item[EKey.contracts].contains(contractAddress)) continue;
 
